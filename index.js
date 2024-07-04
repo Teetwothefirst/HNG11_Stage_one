95d423a6ccc75d47b23baf7a543380a6ee066622
teetwothesecond <159890208+teetwothesecond@users.noreply.github.com>
2024-07-04 16:02:04 -0700

    Changes to the Filter

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let time = document.getElementById("today-date");
 let date = new Date();
-console.log(date)
+time.innerText = date.toUTCString();
+
+let Day_of_week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday'];
+let Day_of_week_select = new Date();
+console.log(Day_of_week_select.getDay())
+console.log(Day_of_week[Day_of_week_select.getDay()- 1]);
