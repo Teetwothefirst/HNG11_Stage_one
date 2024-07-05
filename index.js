c95dc215574faec08a5983dfe957d10bb081f09a
teetwothesecond <159890208+teetwothesecond@users.noreply.github.com>
2024-07-05 03:39:57 -0700

    Changes

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ let Day_of_week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'
 let Day_of_week_select = new Date();
 console.log(Day_of_week_select.getDay())
 console.log(Day_of_week[Day_of_week_select.getDay()- 1]);
+
+
+// function toUTC(){
+//     const useWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//     const newDate = document.getElementById('getDOW')
+//     let d = new Date();
+//     newDate.innerHTML  = useWeek[d.getDay()];
+
+//     const UTCTime = document.getElementById('getTimeUTC');
+//     let u = new Date();
+//     UTCTime.innerHTML = u.toUTCString();
+
+// }
+// toUTC();
